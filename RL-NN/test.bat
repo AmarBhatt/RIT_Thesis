1fcd2621eac8624b8228abd08a396555870f2c75
Amar Bhatt <bhatt.amar.a@gmail.com>
2017-06-10 00:29:26 -0400

    Clean Up

--- RL-NN/test.bat
+++
@@ -1,5 +0,0 @@
-start python run.py 0 500 40 80 10 50 state0 | wtee state0.txt
-start python run.py 1 1000 40 80 10 100 state1 | wtee state1.txt
-start python run.py 2 100000 40 80 5000 10000 state2 | wtee state2.txt
-start python run.py 3 100000 40 80 5000 10000 state3 | wtee state3.txt
-PAUSE
